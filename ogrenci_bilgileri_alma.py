a294e9b6ce5aa0034bda2684fa8ce32c46961eee
Umutcan Tunç <umutcantuncc@gmail.com>
2025-09-25 01:47:30 +0300

    Create ogrenci_bilgileri_alma.py

---
+++ ogrenci_bilgileri_alma.py
@@ -0,0 +1,5 @@
+ad = input("Adınızı girin:")
+soyad =input("Soyadınızı girin:")
+numara = input("Numaranızı girin:")
+bilgiler = [ad,soyad,numara]
+print("Adınız: {}\nSoyadınız: {}\nNumaranız: {}\n".format(bilgiler[0],bilgiler[1],bilgiler[2]))
