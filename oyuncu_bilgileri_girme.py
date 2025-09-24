fcec8a1377d130500ea9bf86cf012ef44fe2cde1
Umutcan Tunç <umutcantuncc@gmail.com>
2025-09-25 01:47:33 +0300

    Create oyuncu_bilgileri_girme.py

---
+++ oyuncu_bilgileri_girme.py
@@ -0,0 +1,8 @@
+print("Oyuncu Kaydetme Programı")
+ad = input("Oyuncu Adı:")
+soyad = input("Oyuncu Soyadı:")
+takım = input("Oyuncu Takımı:")
+bilgiler = [ad,soyad,takım]
+print("Bilgiler kaydediliyor...")
+print("Oyuncu Adı: {}\nOyuncu Soyadı: {}\nOyuncu Takımı: {}\n" .format(bilgiler[0],bilgiler[1],bilgiler[2]))
+print("Bilgiler kaydedildi.")
