c60e934f59a9ba113964496912a1345078646438
Umutcan Tunç <umutcantuncc@gmail.com>
2025-09-25 01:47:03 +0300

    Create ikinci_derece_denklemlerde_kok_bulma.py

---
+++ ikinci_derece_denklemlerde_kok_bulma.py
@@ -0,0 +1,8 @@
+print("ax2+bx+c ifadesindeki a b c ifadelerini yazınız")
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+delta = b**2 - 4*a*c
+x1 = (-b + delta**0.5) / (2*a)
+x2 = (-b - delta**0.5) / (2*a)
+print("Birinci kök: {}\n İkinci Kök: {}".format(x1,x2))
