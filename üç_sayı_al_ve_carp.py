671298955bfa50520f7b6a7aec09adb9038acbba
Umutcan Tunç <umutcantuncc@gmail.com>
2025-09-25 01:47:37 +0300

    Create üç_sayı_al_ve_carp.py

---
+++ üç_sayı_al_ve_carp.py
@@ -0,0 +1,6 @@
+print("Üç Sayı girin ve çarpımlarının sonucunu görün")
+s1=int(input("Birinci sayıyı giriniz:"))
+s2=int(input("İkinci sayıyı giriniz:"))
+s3=int(input("Üçüncü Sayıyı Giriniz:"))
+carpım = s1*s2*s3
+print("{} x {}x {} = {}".format(s1,s2,s3,carpım))
