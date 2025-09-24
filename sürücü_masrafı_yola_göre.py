68bea88b9dc2ff0e4c83c1340656e0342529d8e1
Umutcan Tunç <umutcantuncc@gmail.com>
2025-09-25 01:47:35 +0300

    Create sürücü_masrafı_yola_göre.py

---
+++ sürücü_masrafı_yola_göre.py
@@ -0,0 +1,4 @@
+print("Gidilen Yola Göre Masraf Bulma Programı")
+alinan_yol = int(input("Kaç kilometre yol gittiniz?:"))
+yakılan_litre_bası = int(input("Kilometrede ne kadar yakıyor?:"))
+print("Masrafınız:",alinan_yol*yakılan_litre_bası,"TL")
