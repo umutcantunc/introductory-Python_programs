254c488c6de408856679a2bb3f616b275e3976ce
Umutcan Tunç <umutcantuncc@gmail.com>
2025-09-25 01:47:17 +0300

    Create hipotenus_bulma_programı.py

---
+++ hipotenus_bulma_programı.py
@@ -0,0 +1,5 @@
+print("Hipotenüs Hesaplama Programı")
+a = int(input("Birinci Dik Kenar Uzunluğunu Giriniz:"))
+b = int(input("İkinci Dik Kenar Uzunluğunu Giriniz: "))
+hipotenus = (a**2 + b**2)**0.5
+print("Hipotenüs uzunluğu:",(hipotenus))
